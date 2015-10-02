8dd4617a959833b23342da56bc8b009c0c9759b7
picklesolution <nicholasnavarro@Nicholass-MacBook-Pro.local>
2015-10-01 20:52:05 -0700

    Added Processing Gallery
    new gallery

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,13 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-	$( "#clickthison").click(function() {
-    console.log( "clicked!" );
-    	$("#clickthison").removeClass(".blue");
-    	$("#clickthison").addClass(".red");
+	$( "#redbutton").click(function() {
+    	console.log( "clicked!" );
+    		$("#clickthison").removeClass(".blue");
+    		$("#clickthison").addClass(".red");
+	});
+	$( "#bluebutton").click(function() {
+   	 	console.log( "clicked!" );
+    		$("#clickthison").removeClass(".red");
+    		$("#clickthison").addClass(".blue");
 	});
 });
